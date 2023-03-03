07c2ca22df556ae2bd760526248f2d60312ab653
MateusMoreiraNaN <mateusgodoi117@gmail.com>
2023-03-03 12:59:40 -0300

    subir

--- Usuários/src/routes/index.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express'
-
-import * as HomeController from '../controllers/homeController'
-
-const router = Router()
-
-router.get('/', HomeController.home)
-
-export default router
