44050b5b815fc05f54bdd63a649bb1508c60625c
MateusMoreiraNaN <mateusgodoi117@gmail.com>
2023-03-03 16:04:59 -0300

    Base

--- Projeto02Blog/src/routes/main.ts
+++ Projeto02Blog/src/routes/main.ts
@@ -1,7 +1,8 @@
 import { Router } from 'express'
+import * as HomeController from '../controllers/homeController'
 
 const router = Router()
 
-router.get('/')
+router.get('/', HomeController.home)
 
 export default router
