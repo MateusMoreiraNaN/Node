d7b86581812900b11ee20b37517f0dc1923b75ff
MateusMoreiraNaN <mateusgodoi117@gmail.com>
2023-03-03 14:59:18 -0300

    first commit

---
+++ Projeto02Blog/src/server.ts
@@ -0,0 +1,27 @@
+import express, { Request, Response } from 'express'
+import path from 'path'
+import dotenv from 'dotenv'
+import mainRoutes from './routes/main'
+import mustache from 'mustache-express'
+import bodyParser from 'body-parser'
+
+dotenv.config()
+
+const server = express()
+
+server.set('view engine', 'mustache');
+server.set('views', path.join(__dirname, 'views'));
+server.engine('mustache', mustache());
+
+server.use(express.static(path.join(__dirname, '../public')));
+server.use(express.urlencoded({extended: true}));
+server.use(bodyParser.urlencoded({extended: false}))
+server.use(bodyParser.json())
+
+server.use(mainRoutes);
+
+server.use((req: Request, res: Response)=>{
+    res.status(404).send('Página não encontrada!');
+});
+
+server.listen(process.env.PORT);
