df722c59844bebc8d7e092c557040429515d3b3e
MateusMoreiraNaN <mateusgodoi117@gmail.com>
2023-03-04 11:24:23 -0300

    Category

---
+++ Projeto02Blog/src/models/Category.ts
@@ -0,0 +1,16 @@
+import { Model, DataTypes } from "sequelize"
+import { sequelize } from "../instances/mysql"
+
+export interface CategoryInstance extends Model{
+    title: string
+}
+
+export const Category = sequelize.define<CategoryInstance>("Category",{
+    title:{
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+},{
+    tableName: 'category',
+    timestamps: false
+})
