531a533d6c20e9e166f4477a5213aa692cf0d941
MateusMoreiraNaN <mateusgodoi117@gmail.com>
2023-04-10 18:58:41 -0300

    home

--- Projeto02Blog/src/controllers/homeController.ts
+++ Projeto02Blog/src/controllers/homeController.ts
@@ -1,9 +1,13 @@
 import { Request, Response } from "express";
 import { Op, where } from "sequelize";
+import { Article } from "../models/Articles";
 
 export const home = async (req: Request, res: Response)=>{
 
-    res.render('index',{
+    let articles = await Article.findAll()
+    if(articles){
+        res.render('index',{articles: articles})
+    }
 
-    })
+    
 }
