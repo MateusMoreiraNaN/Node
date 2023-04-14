c34d0c9ed92195abefcbc3c8eee2988591cdca8a
“MateusMoreiraNaN <mateusgodoi117@gmail.com>
2023-04-14 14:03:03 -0300

    article

--- Projeto02Blog/src/controllers/homeController.ts
+++ Projeto02Blog/src/controllers/homeController.ts
@@ -3,12 +3,22 @@ import { Op, where } from "sequelize";
 import { Article } from "../models/Articles";
 import slugify from "slugify";
 import { title } from "process";
+import { Category } from "../models/Category";
 
 export const home = async (req: Request, res: Response)=>{
 
-    let articles = await Article.findAll()
+    let articles = await Article.findAll({
+        order:[
+            ['id','DESC']
+        ]
+    })
     if(articles){
-        res.render('index',{articles: articles})
+        let categories = await Category.findAll()
+        if(categories){
+            res.render('index',{articles: articles, categories: categories})
+        }
+
+        
     }
 
     
@@ -16,20 +26,43 @@ export const home = async (req: Request, res: Response)=>{
 
 export const slug = async (req: Request, res: Response)=>{
     let slug = req.params.slug
+
     
+    if(slug){
+        let article = await Article.findOne({
+            where:{
+                slug: slug
+            }
+        })
+        if(article){
+            let categories = await Category.findAll()
+            if(categories){
+                res.render('article',{article: article, categories: categories})
+            }
+
+    }else{
+        res.redirect("/")
+    }
     
+    /*
     await Article.findOne({
         where:{
             slug: slug
     }}).then(Article =>{
         if(slug){
             //slug
-            res.render("article", {Article: Article})
+            let categories = await Category.findAll()
+        if(categories){
+            res.render('article',{Article: Article, categories: categories})
+        }
         }else{
             res.redirect("/")
         }
     })
-
+    */
 
     
+
+
+    }
 }
