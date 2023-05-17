110f65dc80f4f7055b5dede2c5a5d8edf1b6959a
MateusMoreiraNaN <mateusgodoi117@gmail.com>
2023-05-17 14:31:20 -0300

    promises

---
+++ ProgramaçãoAssincrona/PromisesAninhadas&PromiseHell.js
@@ -0,0 +1,48 @@
+const pegarId = ()=>{
+    return new Promise((resolve, reeject)=>{
+        setTimeout(()=>{
+            resolve(5)
+        },1500)
+    })
+}
+
+const buscarEmailNoBanco = (id)=>{
+    return new Promise((resolve,reeject)=>{
+        setTimeout(()=>{
+            resolve("mateusgodoi@gmail.com")
+        })
+    },2000)
+}
+
+function enviarEmail(corpo, para){
+    return new Promise((resolve, reeject)=>{
+        setTimeout(()=>{
+
+            let deuErro = false
+
+            if(!deuErro){
+                resolve({time: 6, to: 'mateusgodoi54.com'}) // Promessa ok
+            }else{
+                reeject("Fila cheia") // foi mal, eu falhei :(
+            }
+
+
+        }, 4000)
+    })
+}
+
+console.log("Inicio");
+
+pegarId().then((id) =>{
+    buscarEmailNoBanco(id).then((email)=>{
+
+        enviarEmail("Olá, como vai?",email).then(()=>{
+            console.log("Email enviado, para o usuário com id: " + id);
+        }).catch(err =>{
+            console.log(err);
+        })
+
+    })
+})
+
+console.log("Fim");
