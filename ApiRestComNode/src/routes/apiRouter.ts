2cd31adadf1c41de6d772c0fdd273649c87ac80a
MateusMoreiraNaN <mateusgodoi117@gmail.com>
2023-05-21 18:40:03 -0300

    save

---
+++ ApiRestComNode/src/routes/apiRouter.ts
@@ -0,0 +1,8 @@
+import { Router } from "express";
+
+import * as ApiController from '../controllers/apiGameController'
+
+const router = Router()
+
+
+export default router
--- ApiRestComNode/src/server.ts
+++ ApiRestComNode/src/server.ts
@@ -1,4 +1,16 @@
 import express, { Request, Response } from 'express'
 import path from 'path'
 import dotenv from 'dotenv'
+import apiRouter from './routes/apiRouter'
 
+dotenv.config()
+
+const server = express()
+
+server.use((req: Request, res: Response)=>{
+    res.status(404).send('Página não encontrada!')
+})
+
+server.use(apiRouter)
+
+server.listen(process.env.PORT)
