2165f6c84f47a0fc49ba985cfabab4b877657548
MateusMoreiraNaN <mateusgodoi117@gmail.com>
2023-05-21 23:55:43 -0300

    save

---
+++ ApiRestComNode/src/models/games.ts
@@ -0,0 +1,34 @@
+import { Model, DataTypes } from "sequelize";
+import { sequelize } from "../instances/msql";
+
+export interface GamesIntance extends Model{
+    id: number,
+    title: string,
+    year: number,
+    price: number
+}
+
+export const Games = sequelize.define<GamesIntance>("Games",{
+    id:{
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false
+    },
+    title:{
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    year:{
+        type: DataTypes.NUMBER,
+        allowNull: false
+    },
+    price:{
+        type: DataTypes.NUMBER,
+        allowNull: false
+    }
+
+},{
+    tableName: 'games',
+    timestamps: false
+})
