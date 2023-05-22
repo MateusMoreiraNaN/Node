778fad194913f3ecc9a76f5752eb019a8e3f2f0e
MateusMoreiraNaN <mateusgodoi117@gmail.com>
2023-05-22 14:19:42 -0300

    api

--- ApiRestComNode/src/controllers/apiGameController.ts
+++ ApiRestComNode/src/controllers/apiGameController.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from "express";
 import { Games } from "../models/games";
+import { write } from "fs";
+import { where } from "sequelize";
 
 export const game = async(req: Request, res: Response)=>{
     let { title, year, price } = req.body
@@ -15,4 +17,27 @@ export const games = async(req: Request, res: Response)=>{
     let list = await Games.findAll()
 
     res.json({ list })
+}
+
+export const gameId = async(req: Request, res: Response)=>{
+    let id = req.params.id
+    
+    if(isNaN(parseInt(id))){
+        //res.send("Isso não é um número")
+        //res.json({ id })
+        res.sendStatus(400)
+    }else{
+        let id = parseInt(req.params.id)
+
+        let game = await Games.findOne({where:{id: id}})
+
+        if(game != undefined){
+            res.statusCode = 200
+            res.json({game})
+        }else{
+            res.sendStatus(404)
+        }
+    }
+
+    
 }
--- ApiRestComNode/src/routes/apiRouter.ts
+++ ApiRestComNode/src/routes/apiRouter.ts
@@ -6,6 +6,7 @@ const router = Router()
 
 router.post('/game',ApiController.game)
 router.get('/games', ApiController.games)
+router.get('/game/:id', ApiController.gameId)
 
 
 export default router
