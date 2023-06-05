d4d0b1ac3c78b6c91ae4dc3cd623c7d4ecce72f8
MateusMoreiraNaN <mateusgodoi117@gmail.com>
2023-06-05 19:26:44 -0300

    giit

--- ApiRestComNode/src/routes/userRouter.ts
+++ ApiRestComNode/src/routes/userRouter.ts
@@ -4,5 +4,8 @@ import * as UserController from '../controllers/userController'
 
 const router = Router()
 
+router.post('/register', UserController.register)
+router.post('/login', UserController.login)
+
 
 export default router
