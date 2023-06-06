557dcf786ec6bc5cf5a36e32cfec9128cd9829ee
MateusMoreiraNaN <mateusgodoi117@gmail.com>
2023-06-06 13:05:59 -0300

    jwt

--- ApiRestComNode/src/server.ts
+++ ApiRestComNode/src/server.ts
@@ -2,8 +2,10 @@ import express, { Request, Response } from 'express'
 import path from 'path'
 import dotenv from 'dotenv'
 import apiRouter from './routes/apiRouter'
+import ApiUser from './routes/userRouter'
 import bodyParser from 'body-parser'
 import  Cors from 'cors'
+import passport from 'passport'
 
 dotenv.config()
 
@@ -15,7 +17,10 @@ server.use(express.urlencoded({extended: true}));
 server.use(bodyParser.urlencoded({extended: false}))
 server.use(bodyParser.json())
 
+server.use(passport.initialize())
+
 server.use(apiRouter)
+server.use(ApiUser)
 
 server.use((req: Request, res: Response)=>{
     res.status(404).send('Página não encontrada!')
